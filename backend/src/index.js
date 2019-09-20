ac341517e174f14427cc422ae628382339934ca7
Wesley Chang <wesleycanada@gmail.com>
2019-09-20 15:32:47 -0700

    Dockerize backend and add MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const PORT = 8080;
+const MONGO_URL = 'mongodb://171.0.0.3:27017';
 
 const app = express();
+const mongoClient = MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
+mongoClient.connect(err => {
+  if (err) {
+    throw "Database connection failed!";
+  }
+  
+  app.get('/', (req, res) => {
     res.send("Hello World!");
-});
+  });
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
