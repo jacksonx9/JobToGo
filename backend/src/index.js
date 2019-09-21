dcc89049faeb8d81f9829e6ba11876b3859c9505
Wesley Chang <wesleycanada@gmail.com>
2019-09-20 19:51:09 -0700

    Fix async/await

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,24 +7,24 @@ const MONGO_URL = 'mongodb://171.0.0.3:27017';
 const app = express();
 const mongoClient = MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoClient.connect(async err => {
-  if (err) {
-    throw "Database connection failed!";
+mongoClient.connect(async (e) => {
+  if (e) {
+    throw Error('Database connection failed!');
   }
 
-  const db = mongoClient.db("test");
-  const collection = db.collection("test");
+  const db = mongoClient.db('test');
+  const collection = db.collection('test');
   const collectionData = await collection.find({}).toArray();
 
   // If collection is empty, insert Hello World message
   if (collectionData.length === 0) {
-    await collection.insertOne({ message: "Hello World!" }).catch(err => console.log(err));
+    await collection.insertOne({ message: 'Hello World!' }).catch((e) => console.log(e));
   }
-  
+
   // On / access, return Hello World message from db
   app.get('/', async (req, res) => {
-    const collectionData = await collection.find({}).toArray();
-    res.send(collectionData[0].message);
+    const helloWorldData = await collection.find({}).toArray();
+    res.send(helloWorldData[0].message);
   });
 
   app.listen(PORT, () => {
