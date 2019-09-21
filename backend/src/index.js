9fbd26fd5e131d42f1a01658259eeaa7e799635d
Wesley Chang <wesleycanada@gmail.com>
2019-09-20 18:51:29 -0700

    Add mongodb hello world

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,13 +7,24 @@ const MONGO_URL = 'mongodb://171.0.0.3:27017';
 const app = express();
 const mongoClient = MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoClient.connect(err => {
+mongoClient.connect(async err => {
   if (err) {
     throw "Database connection failed!";
   }
+
+  const db = mongoClient.db("test");
+  const collection = db.collection("test");
+  const collectionData = await collection.find({}).toArray();
+
+  // If collection is empty, insert Hello World message
+  if (collectionData.length === 0) {
+    await collection.insertOne({ message: "Hello World!" }).catch(err => console.log(err));
+  }
   
-  app.get('/', (req, res) => {
-    res.send("Hello World!");
+  // On / access, return Hello World message from db
+  app.get('/', async (req, res) => {
+    const collectionData = await collection.find({}).toArray();
+    res.send(collectionData[0].message);
   });
 
   app.listen(PORT, () => {
