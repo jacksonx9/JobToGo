7b9c05d61b28f72a8751148b5af08959286cdad4
Wesley Chang <wesleycanada@gmail.com>
2019-09-20 19:55:23 -0700

    Dockerize backend and add mongodb (#8)
    * Dockerize backend and add MongoDB
    
    * Add mongodb hello world
    
    * Fix async/await

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,33 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const PORT = 8080;
+const MONGO_URL = 'mongodb://171.0.0.3:27017';
 
 const app = express();
+const mongoClient = MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+mongoClient.connect(async (e) => {
+  if (e) {
+    throw Error('Database connection failed!');
+  }
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  const db = mongoClient.db('test');
+  const collection = db.collection('test');
+  const collectionData = await collection.find({}).toArray();
+
+  // If collection is empty, insert Hello World message
+  if (collectionData.length === 0) {
+    await collection.insertOne({ message: 'Hello World!' }).catch((e) => console.log(e));
+  }
+
+  // On / access, return Hello World message from db
+  app.get('/', async (req, res) => {
+    const helloWorldData = await collection.find({}).toArray();
+    res.send(helloWorldData[0].message);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
