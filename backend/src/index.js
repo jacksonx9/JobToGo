2aed252a4d466bd20943ccee2ccf19c4e3868dc2
Wesley Chang <wesleycanada@gmail.com>
2019-10-10 15:03:20 -0700

    Job search, mongo, parsing test code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import JobSearcher from './job_search';
+import ResumeParser from 'resume-parser';
+import fs from 'fs';
+import retext from 'retext';
+import pos from 'retext-pos';
+import keywords from 'retext-keywords';
+import toString from 'nlcst-to-string';
+import pdf from 'pdf-parse';
 
 const PORT = 8080;
 const MONGO_URL = 'mongodb://171.0.0.3:27017';
@@ -11,20 +19,73 @@ mongoClient.connect(async (e) => {
   if (e) {
     throw Error('Database connection failed!');
   }
+  let dataBuffer = fs.readFileSync('Resume.pdf');
 
-  const db = mongoClient.db('test');
-  const collection = db.collection('test');
-  const collectionData = await collection.find({}).toArray();
+  pdf(dataBuffer).then(function(data) {
+    const k = new Set();
+    fs.writeFileSync('Resume-parsed.txt', data.text);
 
-  // If collection is empty, insert Hello World message
-  if (collectionData.length === 0) {
-    await collection.insertOne({ message: 'Hello World!' }).catch((e) => console.log(e));
-  }
+    retext()
+      .use(pos) // Make sure to use `retext-pos` before `retext-keywords`.
+      .use(keywords)
+      .process(data.text, done);
 
-  // On / access, return Hello World message from db
-  app.get('/', async (req, res) => {
-    const helloWorldData = await collection.find({}).toArray();
-    res.send(helloWorldData[0].message);
+    function done(err, file) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      file.data.keywords.forEach(function(keyword) {
+        k.add(toString(keyword.matches[0].node));
+      });
+    }
+    console.log(k);
+  });
+
+  const jobSearcher = new JobSearcher();
+
+  // const db = mongoClient.db('test');
+  // await db.createCollection('test', {
+  //   validator: {
+  //     $jsonSchema: {
+  //       bsonType: Object,
+  //       properties: {
+  //         message: {
+  //           bsonType: String,
+  //         },
+  //         status: {
+  //           enum: ['a', 'b']
+  //         }
+  //       }
+  //     }
+  //   }
+  // }).catch(e => console.log(e));
+  // const testCollection = db.collection('test');
+
+  // await testCollection.insertMany([
+  //   {
+  //     message: 'lol',
+  //     status: 'a',
+  //   },
+  //   {
+  //     message: 'asdf df',
+  //     status: 'b'
+  //   }
+  // ]).catch(e => console.log(e));
+
+  // await testCollection.createIndex({ message: 'text' }).catch(e => console.log(e));
+  // console.log(await testCollection.countDocuments({ status: 'a' }).catch(e => console.log(e)));
+  // console.log(await testCollection.distinct('message').catch(e => console.log(e)));
+
+  // const message = await testCollection.find({
+  //   $text: {
+  //     $search: 'df',
+  //   }
+  // }).project({ _id: 0 }).toArray();
+  // console.log(message);
+
+  app.get('/jobs/:query', async (req, res) => {
+    res.json(await jobSearcher.getJobs(req.params.query));
   });
 
   app.listen(PORT, () => {
