973abf02010179516d975ac6bf74b78aff7e812d
jacksonx9 <yifan-xie@hotmail.com>
2019-10-12 23:09:37 -0700

    user schema done

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,41 +16,102 @@ mongoClient.connect(async (e) => {
     throw Error('Database connection failed!');
   }
    
-  const db = mongoClient.db('jobs');
-  await db.createCollection('jobs', {
+  const jobsDB = mongoClient.db('jobs');
+  await jobsDB.createCollection('jobs', {
     validator: {
       $jsonSchema: {
         bsonType: Object,
         properties: {
           title: {
-            bsonType: String,
+            bsonType: String
           },
           companyName: {
-            bsonType: String,
+            bsonType: String
           },
           location: {
-            bsonType: String,
+            bsonType: String
           },
           url: {
-            bsonType: String,
+            bsonType: String
           },
           postDate: {
-            bsonType: String,
+            bsonType: String
           },
           salary: {
-            bsonType: String,
+            bsonType: String
           },
           description: {
-            bsonType: String,
+            bsonType: String
           }
         }
       }
     }
   }).catch(e => console.log(e));
 
-  const jobSearcher = new JobSearcher(db);
-  const jobAnalyzer = new JobAnalyzer(db);
+  const jobSearcher = new JobSearcher(jobsDB);
+  const jobAnalyzer = new JobAnalyzer(jobsDB);
 
+  const usersDB = mongoClient.db('user');
+  await usersDB.createCollection('user', {
+    validator: {
+      $jsonSchema: {
+        bsonType: Object,
+        properties: {
+          credentials: {
+            bsonType: Object,
+            properties: {              
+              userName: { bsonType: String },
+              email: { bsonType: String },
+              password: { bsonType: String }
+            }            
+          },
+          userInfo: {
+            bsonType: Object,
+            properties: {              
+              location: { bsonType: String },
+              jobType: { enum: [ 'full-time', 'part-time', 'internship', 'unspecified' ] },
+              skillsExperiences: { bsonType: Array }
+            }            
+          },
+          keywords: {
+            bsonType: Array,
+            properties: {    
+              keywordStatistic: {
+                bsonType: Object,
+                properties: {
+                  score: { bsonType: 'int' },
+                  jobCount: { bsonType: 'int' },
+                  timeStamp: { bsonType: 'timestamp' }
+                }
+              }          
+            }            
+          },
+          friends: {
+            bsonType: Array,
+            properties: {
+              friendId: { bsonType: 'int' }
+            }
+          },
+          jobShortList: {
+            bsonType: Array,
+            properties: {
+              jobId: { bsonType: 'int' }
+            }
+          },
+          resumePath: {
+            bsonType: String,
+          }
+        }
+      }
+    }
+  }).catch(e => console.log(e));
+  const uersCollection = usersDB.collection('user');
+
+  await uersCollection.insertMany([
+    {
+      resumePath: 'test'
+    }
+  ]).catch(e => console.log(e));
 
 
   //purely for testing adding to database
