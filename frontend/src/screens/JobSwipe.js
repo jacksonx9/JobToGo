e462322a9df1d45c6e84ebed8df3d5d9505adb17
viickif <vicki.wfu@gmail.com>
2019-10-12 17:29:14 -0700

    job swipe ready to connect to server

--- frontend/src/screens/JobSwipe.js
+++ frontend/src/screens/JobSwipe.js
@@ -3,6 +3,7 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import axios from 'axios';
  
 class JobSwipe extends Component {
  
@@ -11,27 +12,37 @@ class JobSwipe extends Component {
     this.state = {
       myText: 'I\'m ready to get swiped!',
       gestureName: 'none',
-      backgroundColor: '#fff'
+      backgroundColor: '#fff',
+      jobs: '',
+      jobIndex: 0
     };
   }
+
+  async componentDidMount() {
+    const jobs = await axios.get('http://10.0.0.42:8080/jobs/javascript').catch(e => console.log(e));
+    this.setState({
+      jobs: jobs.data,
+    })
+  }
  
-  onSwipeUp(gestureState) {
+  onSwipeUp = (gestureState) => {
     this.setState({myText: 'You swiped up!'});
   }
- 
-  onSwipeDown(gestureState) {
+
+  onSwipeDown = (gestureState) => {
     this.setState({myText: 'You swiped down!'});
   }
- 
-  onSwipeLeft(gestureState) {
+
+  onSwipeLeft = (gestureState) => {
     this.setState({myText: 'You swiped left!'});
   }
- 
-  onSwipeRight(gestureState) {
+
+  onSwipeRight = (gestureState) => {
     this.setState({myText: 'You swiped right!'});
+    this.setState({jobIndex: this.state.jobIndex +1})
   }
- 
-  onSwipe(gestureName, gestureState) {
+
+  onSwipe = (gestureName, gestureState) => {
     const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
     this.setState({gestureName: gestureName});
     switch (gestureName) {
@@ -49,6 +60,7 @@ class JobSwipe extends Component {
         break;
     }
   }
+
  
   render() {
  
@@ -72,6 +84,7 @@ class JobSwipe extends Component {
         >
         <Text>{this.state.myText}</Text>
         <Text>onSwipe callback received gesture: {this.state.gestureName}</Text>
+        <Text>Job info: {this.state.jobs[this.state.jobIndex].title}</Text>
       </GestureRecognizer>
     );
   }
