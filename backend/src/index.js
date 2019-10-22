fc1571b5867a89043f7be8a4c443843c95a41674
jacksonx9 <yifan-xie@hotmail.com>
2019-10-22 16:04:05 -0700

    finish merge

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,8 @@ import JobSearcher from './job_searcher';
 import User from './user';
 import ResumeParser from './resume_parser';
 import JobAnalyzer from './job_analyzer';
-<<<<<<< HEAD
 import JobShortLister from './job_shortlister';
 import Messenger from './messenger';
-=======
->>>>>>> 19436d7... remove works and clean up code
 
 
 const PORT = 8080;
