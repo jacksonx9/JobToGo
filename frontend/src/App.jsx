645593785283e47078b1ab6c3fe66834071e1ea4
viickif <vicki.wfu@gmail.com>
2019-10-31 20:22:10 -0700

    added logger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import firebase from 'react-native-firebase';
+import { logger } from 'react-native-logger';
 
 import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
@@ -22,25 +23,25 @@ export default class App extends React.Component {
         await firebase.messaging().requestPermission();
         const fcmToken = await firebase.messaging().getToken();
         if (fcmToken) {
-          console.log('FCM Token: ', fcmToken);
+          logger.log('FCM Token: ', fcmToken);
           global.firebaseToken = fcmToken;
           const enabled = await firebase.messaging().hasPermission();
           if (enabled) {
-            console.log(`FCM messaging has permission:${enabled}`);
+            logger.log(`FCM messaging has permission:${enabled}`);
           } else {
             try {
               await firebase.messaging().requestPermission();
-              console.log('FCM permission granted');
+              logger.log('FCM permission granted');
             } catch (error) {
-              console.log('FCM Permission Error', error);
+              logger.log('FCM Permission Error', error);
             }
           }
           this.createNotificationListeners();
         } else {
-          console.log('FCM Token not available');
+          logger.log('FCM Token not available');
         }
       } catch (e) {
-        console.log('Error initializing FCM', e);
+        logger.log('Error initializing FCM', e);
       }
     }
   }
@@ -54,7 +55,7 @@ export default class App extends React.Component {
     alert(
       title, body,
       [
-        { text: 'OK', onPress: () => console.log('OK Pressed') },
+        { text: 'OK', onPress: () => logger.log('OK Pressed') },
       ],
       { cancelable: false },
     );
@@ -70,28 +71,11 @@ export default class App extends React.Component {
     });
 
     /*
-    * If your app is in background, you can listen for when a notification
-    * is clicked / tapped / opened as follows:
-    * */
-    this.notificationOpenedListener = firebase.notifications()
-      .onNotificationOpened((notificationOpen) => {
-        const { title, body } = notificationOpen.notification;
-      });
-
-    /*
-    * If your app is closed, you can check if it was opened by a notification
-    * being clicked / tapped / opened as follows:
-    * */
-    const notificationOpen = await firebase.notifications().getInitialNotification();
-    if (notificationOpen) {
-      const { title, body } = notificationOpen.notification;
-    }
-    /*
     * Triggered for data only payload in foreground
     * */
     this.messageListener = firebase.messaging().onMessage((message) => {
       // process data message
-      console.log(JSON.stringify(message));
+      logger.log(JSON.stringify(message));
     });
   }
 
