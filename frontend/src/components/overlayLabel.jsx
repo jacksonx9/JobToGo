c15916c5b7e82c935918fb4969aee87d9228297b
michaelfu1029 <michaelfu1029@gmail.com>
2019-11-03 14:38:59 -0800

    Swiping animation and labels implemented

--- frontend/src/components/overlayLabel.jsx
+++ frontend/src/components/overlayLabel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { string } from 'prop-types'
 import { overlayLabelStyles } from '../styles';
-// import styles from './OverlayLabel.styles'
+
 
 const OverlayLabel = ({ label, color }) => (
   <View style={[overlayLabelStyles.overlayLabel, { borderColor: color }]}>
