38bd659336cf8d564a2edb7f5d161265c8ab7849
jacksonx9 <42988793+jacksonx9@users.noreply.github.com>
2019-11-05 00:13:30 -0800

    finished implementing smaller sample size (#82)
    * finished implementing smaller sample size
    
    * fix spec
    
    * import mongoose.Types only

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,3 +1,5 @@
 export const JOBS_PER_SEND = 20;
+export const JOBS_SEARCH_MAX_SIZE = 2000;
+export const JOBS_SEARCH_PERCENT_SIZE = 0.25;
 export const MIN_JOBS_IN_DB = 500;
 export const MEDIA_ROOT = 'mediafiles/';
