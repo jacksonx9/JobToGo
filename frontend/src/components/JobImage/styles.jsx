d37751afdd71f855c2ce2af0c81da008e0cedb2c
viickif <vicki.wfu@gmail.com>
2019-11-15 02:34:39 -0800

    modified job image use

--- frontend/src/components/JobImage/styles.jsx
+++ frontend/src/components/JobImage/styles.jsx
@@ -3,17 +3,7 @@ import { StyleSheet } from 'react-native';
 import { padding, border, colours } from '../../styles';
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: padding.lg,
-    height: 270,
-    width: '100%',
-    backgroundColor: colours.white,
-  },
   companyLogo: {
-    height: 200,
-    width: 200,
     borderRadius: border.radius,
   },
 });
