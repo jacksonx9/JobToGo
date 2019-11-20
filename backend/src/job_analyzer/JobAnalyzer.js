813c93d30b38224d3d5036a4f4557b7ba3e7a05b
jacksonx9 <yifan-xie@hotmail.com>
2019-11-20 15:36:51 -0800

    restyle

--- backend/src/job_analyzer/JobAnalyzer.js
+++ backend/src/job_analyzer/JobAnalyzer.js
@@ -34,11 +34,9 @@ class JobAnalyzer {
    * @param {Job} job
    */
   computeJobKeywordCount(job, keywords) {
-    // Add the number of occurance of all keywords of the result
     const description = job.description.replace(/^[a-z ]/gi, ' ');
+    // Add the number of occurance of all keywords in the description
     keywords.forEach((keyword) => {
-      // TODO: matches "java" with "javascript" from description
-      // NOTE: if you map with spaces around it, problems such as "java," being excluded arise
       const re = new RegExp(` ${keyword} `, 'gi');
       job.keywords.push({
         name: keyword,
